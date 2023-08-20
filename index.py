e6217eeb4be6197827ef98ebb6c57128c5692e03
Christofeee <117144492+Christofeee@users.noreply.github.com>
2023-08-20 16:14:33 +0630

    Title added

--- index.py
+++ index.py
@@ -1,31 +1,38 @@
 import streamlit as st
 
 #title - used to add the title of an app
-st.title("Muwahahhahaha! Here my shitty test")
+st.title("Group 8")
 
 #header
-st.header("Mmm.. A cAlcUlAtOr? ..")
+st.header("Income Predictor")
 
-#subheader
-st.subheader("bae jus said tht she hates me T.T ... hee she said it in cute way tho keke")
 
-#information 
-st.info("SO yeah THIS IS MY SHIT down there")
 
-#warning
-st.warning("U r going to Die in 3mins . . ....")
 
-#write
-st.write("write a name tht u wanna kill here. .. .. .x")
 
-n1 = st.number_input('Number a:')
-st.title("+")
-n2 = st.number_input('Number b:')
-st.title("=")
 
-result = n1+n2
 
-st.write('the combination of ',n1,' and ',n2,' is ',result)
+
+# #subheader
+# st.subheader("bae jus said tht she hates me T.T ... hee she said it in cute way tho keke")
+
+# #information 
+# st.info("SO yeah THIS IS MY SHIT down there")
+
+# #warning
+# st.warning("U r going to Die in 3mins . . ....")
+
+# #write
+# st.write("write a name tht u wanna kill here. .. .. .x")
+
+# n1 = st.number_input('Number a:')
+# st.title("+")
+# n2 = st.number_input('Number b:')
+# st.title("=")
+
+# result = n1+n2
+
+# st.write('the combination of ',n1,' and ',n2,' is ',result)
 
 
 
